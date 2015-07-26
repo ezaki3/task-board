fa2aaf0dbde2723bcb05555fe00abdfe9a066b7b
kawamura hryk <kawamura.hryk@gmail.com>
2015-07-26 14:02:03 +0900

    Move `groups` controller into `Api::v1` module (#16)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api, { format: 'json' } do
+  namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :groups
-      resources :tasks, except: [:new, :edit]
+      resources :groups, except: [:new, :edit], constraints: {id: /[0-9]+/}
+      resources :tasks, except: [:new, :edit], constraints: {id: /[0-9]+/}
     end
   end
 end
