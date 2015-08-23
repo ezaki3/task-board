1622f4c2730b67ae53ec44e114c1f78d80c309ca
kawamura hryk <kawamura.hryk@gmail.com>
2015-08-23 17:59:56 +0900

    Modify group api route to have nested tasks resources (#44)

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
+  def index
+    super
+    if params[:group_id]
+      group = Group.find(params[:group_id]) # Raise error here if group is not exist
+      @tasks = @tasks.where(group_id: group.id)
+    end
+  end
+
+  def show
+    if params[:group_id]
+      @task = Task.find_by!(id: params[:id], group_id: params[:group_id])
+    else
+      super
+    end
+  end
 end
