aad7de94b0e63e416c97b434bbdb2386dfee7620
Masaru Ezaki <ezaki3@gmail.com>
2015-09-21 14:24:22 +0900

    Change alert close from bootstrap to knockout (#71)

--- js/src/viewmodel/baseviewmodel.js
+++ js/src/viewmodel/baseviewmodel.js
@@ -1,6 +1,14 @@
 var BaseViewModel = function () {
     this.alertSuccessMessage = ko.observable();
     this.alertErrorMessage = ko.observable();
+
+    this.closeAlertSuccess = function () {
+        this.alertSuccessMessage(null);
+    }.bind(this);
+
+    this.closeErrorSuccess = function () {
+        this.alertErrorMessage(null);
+    }.bind(this);
 };
 
 module.exports = BaseViewModel;
