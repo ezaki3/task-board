b0464a71c12ec43f15f4830806e7f81a06fa60f0
kawamura hryk <kawamura.hryk@gmail.com>
2015-11-02 14:11:12 +0900

    Implement going to `back_url` after logged in (#86)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) ||
            User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url # , :notice => 'Signed in!'
+    redirect_to params[:back_url]
   end
 
   def destroy
